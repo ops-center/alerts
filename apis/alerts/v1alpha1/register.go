942a0211a67d6270af96606bb3bd5d9b4cb2d9e1
SajjadSadi074 <sajjadsadi2203@gmail.com>
2026-02-05 10:45:41 +0600

    add oracle alert
    Signed-off-by: SajjadSadi074 <sajjadsadi2203@gmail.com>

--- apis/alerts/v1alpha1/register.go
+++ apis/alerts/v1alpha1/register.go
@@ -78,6 +78,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&MysqlAlertsList{},
 		&OpenFGAAlerts{},
 		&OpenFGAAlertsList{},
+		&OracleAlerts{},
+		&OracleAlertsList{},
 		&PerconaxtradbAlerts{},
 		&PerconaxtradbAlertsList{},
 		&PgbouncerAlerts{},
